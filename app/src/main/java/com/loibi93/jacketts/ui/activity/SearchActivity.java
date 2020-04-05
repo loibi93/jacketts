f84bc85509ba7cfd26f78c43b1dd34ecbf8915e2
Lukas Loibnegger <lukas.loibnegger93@gmail.com>
2020-04-05 21:38:19 +0200

    Implement basic search bar

--- app/src/main/java/com/loibi93/jacketts/ui/activity/SearchActivity.java
+++ app/src/main/java/com/loibi93/jacketts/ui/activity/SearchActivity.java
@@ -1,6 +1,7 @@
 package com.loibi93.jacketts.ui.activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -8,8 +9,10 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 import com.loibi93.jacketts.R;
 import com.loibi93.jacketts.ui.adapter.SectionsPagerAdapter;
+import com.loibi93.jacketts.ui.view.SearchBar;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements SearchBar.OnSearchListener {
+    private SearchBar searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,30 @@ public class SearchActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tab_layout);
         tabs.setupWithViewPager(viewPager);
+
+        searchBar = findViewById(R.id.search_bar);
+        searchBar.setOnSearchListener(this);
+    }
+
+    @Override
+    public void onSearch(String query) {
+        Toast.makeText(this, "Searching for: " + query, Toast.LENGTH_SHORT).show();
+        (new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        searchBar.notifySearchComplete();
+                    }
+                });
+            }
+        })).start();
     }
 }
